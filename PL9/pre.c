393f0f49179de622ee4ec79e472d411e3073453e
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2018-06-28 13:43:00 +0900

    13:42

--- PL9/pre.c
+++ PL9/pre.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
+
 int main() {
   void  *h = dlopen("libtoday.so", RTLD_NOW);
   if (NULL == h){
+    printf("EEEEEEEEEE\n");
+    /*
     fprintf(stderr, "faild%s\n", RTDL_NOW());
-    exit(-1);
+    exit(-1);*/
   }
   printf("h----%p\n",h );
-  void *p = dlsym(h,"*today()");
+  void *p = dlsym(h,"today");
   printf("p----%p\n",p );
-  char (*p_today) = p;
-  char str = (*p_today);
+  char *(*p_today)() = p;
+  char  *str = (*p_today)();
   printf("%s\n", str);
 }
