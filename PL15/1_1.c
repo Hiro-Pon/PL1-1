f27e505858411d386db722e6d81f024a0fb3e2fa
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2018-07-27 14:01:42 +0900

    14:01

--- PL15/1_1.c
+++ PL15/1_1.c
@@ -126,41 +126,38 @@ double* matmul4(double* A, double *B){
   }
 
   start = clock();
-  printf("AA\n");
   for (int k = 0; k < N; k++){
     for (int j = 0; j < N; j+= 4){
       int j1 = j + 1;
       int j2 = j + 2;
       int j3 = j + 3;
-      int j4 = j + 4;
       for (int i = 0; i < N; i += 4){
         int i1 = i + 1;
         int i2 = i + 2;
         int i3 = i + 3;
-        int i4 = i + 4;
+
+        C[i+ N*k] += A[i1+N*j]*B[j+N*k];
+        C[i+ N*k] += A[i1+N*j1]*B[j1+N*k];
+        C[i+ N*k] += A[i1+N*j2]*B[j2+N*k];
+        C[i+ N*k] += A[i1+N*j3]*B[j3+N*k];
+
+        C[i1+ N*k] += A[i1+N*j]*B[j+N*k];
         C[i1+ N*k] += A[i1+N*j1]*B[j1+N*k];
-        C[i2+ N*k] += A[i2+N*j1]*B[j1+N*k];
-        C[i3+ N*k] += A[i3+N*j1]*B[j1+N*k];
-        C[i4+ N*k] += A[i4+N*j1]*B[j1+N*k];
-
         C[i1+ N*k] += A[i1+N*j2]*B[j2+N*k];
-        C[i2+ N*k] += A[i2+N*j2]*B[j2+N*k];
-        C[i3+ N*k] += A[i3+N*j2]*B[j2+N*k];
-        C[i4+ N*k] += A[i4+N*j2]*B[j2+N*k];
-
         C[i1+ N*k] += A[i1+N*j3]*B[j3+N*k];
-        C[i2+ N*k] += A[i2+N*j3]*B[j3+N*k];
-        C[i3+ N*k] += A[i3+N*j3]*B[j3+N*k];
-        C[i4+ N*k] += A[i4+N*j3]*B[j3+N*k];
 
-        C[i1+ N*k] += A[i1+N*j4]*B[j3+N*k];
-        C[i2+ N*k] += A[i2+N*j4]*B[j3+N*k];
-        C[i3+ N*k] += A[i3+N*j4]*B[j3+N*k];
-        C[i4+ N*k] += A[i4+N*j4]*B[j3+N*k];
+        C[i2+ N*k] += A[i1+N*j]*B[j+N*k];
+        C[i2+ N*k] += A[i2+N*j1]*B[j1+N*k];
+        C[i2+ N*k] += A[i2+N*j2]*B[j2+N*k];
+        C[i2+ N*k] += A[i2+N*j3]*B[j3+N*k];
+
+        C[i3+ N*k] += A[i3+N*j]*B[j+N*k];
+        C[i3+ N*k] += A[i3+N*j1]*B[j1+N*k];
+        C[i3+ N*k] += A[i3+N*j2]*B[j2+N*k];
+        C[i3+ N*k] += A[i3+N*j3]*B[j3+N*k];
       }
     }
   }
-  printf("BB\n");
 
   end = clock();
   printf("matmul1: Excution Time: %lf [sec]\n", (double)(end - start)/CLOCKS_PER_SEC);
